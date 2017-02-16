732387cf379f3909277620932d396602a2b6ccbe
seergi93 <sergiroberti93@gmail.com>
2017-02-16 16:58:41 +0100

    PoolConductores start ok

--- SergiRobertiVallori/src/conductores/PoolConductores.java
+++ SergiRobertiVallori/src/conductores/PoolConductores.java
@@ -10,5 +10,17 @@ package conductores;
  * @author Sergi
  */
 public class PoolConductores {
-    
+
+    private Conductor[] poolConducores = null;
+
+    public PoolConductores() {
+    }
+
+    public PoolConductores(Conductor[] poolConducores) {
+        this.poolConducores = poolConducores;
+    }
+
+//    
+//    El constructor PoolConductores(conductores) recibe un array de longitud variable de conductores.
+//asignarConductor()selecciona un conductor libre entre la flota y lo devuelve, estableciendo que ese conductor está ahora ocupado.
 }
