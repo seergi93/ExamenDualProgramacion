b7d5c4e0f9983e31501cc2163d4a9ca609123783
seergi93 <sergiroberti93@gmail.com>
2017-02-16 16:47:28 +0100

    Carrera start ok

--- SergiRobertiVallori/src/carrera/Carrera.java
+++ SergiRobertiVallori/src/carrera/Carrera.java
@@ -10,5 +10,31 @@ package carrera;
  * @author Sergi
  */
 public class Carrera {
-    
+
+    private double tiempoEsperado = 0d;
+    private double tiempoCarrera = 0d;
+    private double costeTotal = 0d;
+    private String conductor = null;
+
+    public Carrera() {
+
+    }
+
+    public Carrera(double tiempoEsperado, double tiempoCarrera, double costeTotal, String conductor) {
+        this.tiempoEsperado = tiempoEsperado;
+        this.tiempoCarrera = tiempoCarrera;
+        this.costeTotal = costeTotal;
+        this.conductor = conductor;
+    }
+
+    //    getTarjetaCredito() devuelve el número de la tarjeta de crédito del usuario/a.
+//getOrigen() devuelve el lugar de origen del trayecto.
+//getDestino() devuelve el lugar de destino del trayecto.
+//getDistancia() devuelve la distancia entre el origen y el destino.
+//getCosteEsperado() devuelve el coste esperado del trayecto, cuyo cálculo es responsabilidad de la clase Tarifa.
+//asignarConductor(PoolConductores conductores) recibe la flota de conductores y asigna un conductor a la carrera. Le pide a la clase PoolConductores que le asigne un conductor.
+//getCosteEsperado() le pregunta a la clase Tarifa cuál es el coste total esperado.
+//realizarPago(pago) recibe el pago y lo almacena en el atributo costeTotal
+//recibirPropina(propina) recibe la propina que paga el usuario
+//liberarConductor() establece que el conductor asignado a la carrera queda libre tras el servicio.
 }
