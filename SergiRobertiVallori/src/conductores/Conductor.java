d747bbbc34902b9af384df7e6516c312be9ed47a
seergi93 <sergiroberti93@gmail.com>
2017-02-16 16:54:51 +0100

    Conductor tart ok

--- SergiRobertiVallori/src/conductores/Conductor.java
+++ SergiRobertiVallori/src/conductores/Conductor.java
@@ -10,5 +10,25 @@ package conductores;
  * @author Sergi
  */
 public class Conductor {
-    
+
+    private String nombre;
+    private String modelo;
+    private String matricula;
+    private double valoracionMedia;
+    private double valoraciones;
+    private boolean ocupado;
+
+    public Conductor() {
+    }
+
+    public Conductor(String nombre, String modelo, String matricula, double valoracionMedia, double valoraciones, boolean ocupado) {
+        this.nombre = nombre;
+        this.modelo = modelo;
+        this.matricula = matricula;
+        this.valoracionMedia = valoracionMedia;
+        this.valoraciones = valoraciones;
+        this.ocupado = ocupado;
+    }
+
+    //setValoracion(valoracion) añade la nueva valoración y actualiza la valoración media del conductor.
 }
