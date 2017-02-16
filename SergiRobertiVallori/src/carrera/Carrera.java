9320c32add823114fc44f5f394e2a26669612949
seergi93 <sergiroberti93@gmail.com>
2017-02-16 17:22:16 +0100

    contructores get & set

--- SergiRobertiVallori/src/carrera/Carrera.java
+++ SergiRobertiVallori/src/carrera/Carrera.java
@@ -11,10 +11,19 @@ package carrera;
  */
 public class Carrera {
 
+    
     private double tiempoEsperado = 0d;
     private double tiempoCarrera = 0d;
     private double costeTotal = 0d;
     private String conductor = null;
+    private String origen;
+    private String destino;
+    private double distancia;
+    private String tarjetaDeCredito;
+    
+    /*
+    CONSTRUCORES
+    */
 
     public Carrera() {
 
@@ -27,14 +36,88 @@ public class Carrera {
         this.conductor = conductor;
     }
 
+    public Carrera(String tarjetaDeCredito) {
+        this.tarjetaDeCredito = tarjetaDeCredito;
+    }
+    
+    
+
     //    getTarjetaCredito() devuelve el número de la tarjeta de crédito del usuario/a.
+ 
+    
 //getOrigen() devuelve el lugar de origen del trayecto.
+    
+   
+    
+    
+    
 //getDestino() devuelve el lugar de destino del trayecto.
+    
+    
 //getDistancia() devuelve la distancia entre el origen y el destino.
+    
+    
 //getCosteEsperado() devuelve el coste esperado del trayecto, cuyo cálculo es responsabilidad de la clase Tarifa.
-//asignarConductor(PoolConductores conductores) recibe la flota de conductores y asigna un conductor a la carrera. Le pide a la clase PoolConductores que le asigne un conductor.
-//getCosteEsperado() le pregunta a la clase Tarifa cuál es el coste total esperado.
+    
+    
+//asignarConductor(PoolConductores conductores) recibe la flota de conductores y
+    //asigna un conductor a la carrera. Le pide a la clase PoolConductores que le asigne un conductor.
+    
+    
+    
+public double getCosteEsperado() {
+    
+    
+    
+}
 //realizarPago(pago) recibe el pago y lo almacena en el atributo costeTotal
 //recibirPropina(propina) recibe la propina que paga el usuario
 //liberarConductor() establece que el conductor asignado a la carrera queda libre tras el servicio.
+            
+            
+            /*
+            GETTERS Y SETTERS
+            */
+
+    public void setTiempoEsperado(double tiempoEsperado) {
+        this.tiempoEsperado = tiempoEsperado;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+
+    public double getTiempoEsperado() {
+        return tiempoEsperado;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public String getTarjetaDeCredito() {
+        return tarjetaDeCredito;
+    }
+    
+    
+            
+    
+            
+            
 }
