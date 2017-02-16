5834d46532b6608ff580b6cb14e3f0b1f44e2140
seergi93 <sergiroberti93@gmail.com>
2017-02-16 16:52:15 +0100

    Tarifa start ok

--- SergiRobertiVallori/src/tarifa/Tarifa.java
+++ SergiRobertiVallori/src/tarifa/Tarifa.java
@@ -5,10 +5,35 @@
  */
 package tarifa;
 
+import java.util.logging.Logger;
+
 /**
  *
  * @author Sergi
  */
 public class Tarifa {
+
+    private String id = null;
+    private double costeMilla = 0d;
+    private double costeMinuto = 0d;
+    private double costeMinimo = 0d;
+    private double porcentajeComision = 0d;
+
+    public Tarifa() {
+    }
+
+    public Tarifa(String id, double costeMilla, double costeMinuto, double costeMinimo, double porcentajeComision) {
+        this.id = id;
+        this.costeMilla = costeMilla;
+        this.costeMinuto = costeMinuto;
+        this.costeMinimo = costeMinimo;
+        this.porcentajeComision = porcentajeComision;
+    }
+
+//    getCosteDistancia(distancia) devuelve la parte del coste del trayecto debido al a distancia.
+//getCosteTiempo(minutos) devuelve la parte del coste del trayecto debido a su duración en minutos.
+//getCosteTotalEsperado(carrera) devuelve el coste total esperado de la carrera que recibe en función de la distancia esperada
+//y el tiempo esperado. El coste total esperado no puede ser inferior al mínimo.
+//    
     
 }
